b2555b6338a31c83c1ee936b5111b818c682dc93
haythamlabrini <haythamlabrini@users.noreply.github.com>
2023-03-16 22:49:42 +0000

    deploy: 5c43692b21f7cf9b65e7b84805cfd98d278461a3

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    './_drafts/**/*.html',
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/**/*.html',
+    './_drafts/*.md',
+    './*.md',
+    './*.html',
+  ],
+  theme: {
+    theme: {
+      extend: {},
+    },
+  },
+  plugins: []
+}
